86f67316466898f6eefc60df3e45d5df02908f56
rgisler <rgisler@swissonline.ch>
2011-12-30 17:08:01 +0100

    Inital Commit

---
+++ src/main/java/ch/gitik/ecos/ConnectionEcosMock.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2012 Roland Gisler, GISLER iNFORMATiK, Switzerland.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id: AbstractBpmsTask.java 38 2007-02-12 12:50:46Z rog $
+ */
+package ch.gitik.ecos;
+
+/**
+ * Simulation der Ecos fuer Testing.
+ * @author Roland Gisler
+ */
+public class ConnectionEcosMock extends Connection {
+
+   @Override
+   public String request(String command) {
+      String result = "<bad request>";
+      if (command.contains("get(1,info)")) {
+         result = "<REPLY get(1, info)>\n1 ECoS\n1 ProtocolVersion[0.1]\n1 ApplicationVersion[1.0.1]\n1 HardwareVersion[1.3]\n<END 0 (OK)>";
+      }
+      return result;
+   }
+
+}
---
+++ src/main/java/ch/gitik/ecos/model/Ecos.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012 Roland Gisler, GISLER iNFORMATiK, Switzerland.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.gitik.ecos.model;
+
+/**
+ * Datenmodell fuer Ecos-Zentrale.
+ * @author Roland Gisler
+ */
+public final class Ecos {
+
+   private final String name;
+
+   private final String hardwareVersion;
+
+   private final String softwareversion;
+
+   private final boolean active;
+
+   /**
+    * Konstruktur.
+    * @param aName
+    *           Name der Zentrale.
+    * @param hwVersion
+    *           Version der Hardware.
+    * @param swVersion
+    *           Version der Software.
+    * @param isActive
+    *           Zentrale aktiv oder nicht.
+    */
+   public Ecos(final String aName, final String hwVersion, final String swVersion, final boolean isActive) {
+      this.name = aName;
+      this.hardwareVersion = hwVersion;
+      this.softwareversion = swVersion;
+      this.active = isActive;
+   }
+
+   /**
+    * Liefert name zurück.
+    * @return name
+    */
+   public String getName() {
+      return name;
+   }
+
+   /**
+    * Liefert hardwareVersion zurück.
+    * @return hardwareVersion
+    */
+   public String getHardwareVersion() {
+      return hardwareVersion;
+   }
+
+   /**
+    * Liefert softwareversion zurück.
+    * @return softwareversion
+    */
+   public String getSoftwareversion() {
+      return softwareversion;
+   }
+
+   /**
+    * Liefert active zurück.
+    * @return active
+    */
+   public boolean isActive() {
+      return active;
+   }
+
+}
