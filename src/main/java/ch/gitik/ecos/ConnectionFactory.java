73b423853f5fd2d82e610c4db4c050b95fdf9f4a
rgisler <rgisler@swissonline.ch>
2012-01-02 16:04:19 +0100

    Dateiheader korrigiert

--- src/main/java/ch/gitik/ecos/ConnectionFactory.java
+++ src/main/java/ch/gitik/ecos/ConnectionFactory.java
@@ -12,13 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * $Id: AbstractBpmsTask.java 38 2007-02-12 12:50:46Z rog $
  */
 package ch.gitik.ecos;
 
 /**
- * 
+ * Factory fuer Connection-Objekte.
  * @author Roland Gisler
  */
 public class ConnectionFactory {
